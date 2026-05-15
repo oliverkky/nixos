6f842d0461c36d231ede5dc70ba1edf9ba924bd2
OliverKKY <oklink@seznam.cz>
2026-05-15 13:55:03 +0200

    Polish desktop transparency and bar spacing

--- home/modules/dev.nix
+++ home/modules/dev.nix
@@ -39,6 +39,8 @@
       scrollback_lines = 10000;
       enable_audio_bell = false;
       confirm_os_window_close = 0;
+      background_opacity = "0.88";
+      dynamic_background_opacity = true;
     };
   };
 
